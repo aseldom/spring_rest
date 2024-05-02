4b01fd5dd2cdc711f03adac78e403e4645904df3
aseldom <a.seldom@gmail.com>
2024-05-02 08:00:05 +0500

    add getEmployee by id

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
@@ -4,6 +4,7 @@ import com.zaurtregulov.spring.rest.entity.Employee;
 import com.zaurtregulov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,11 @@ public class MyRestController {
     @GetMapping("/employees")
     public List<Employee> showAllEmployees(){
         return employeeService.getAllEmployee();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable("id") int id){
+        return employeeService.getEmployee(id);
+
     }
 }
